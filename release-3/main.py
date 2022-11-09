2e06919e5484886b13e38023009c7d1984d38fb1
vladislove <115663088+piligrimfox@users.noreply.github.com>
2022-11-10 02:09:16 +0300

    Update main.py

--- release-3/main.py
+++ release-3/main.py
@@ -1,2 +1,26 @@
-def fullName(obj):
-    return 
+#функция fullName
+def BokLi():
+    my_dictionary = {'name': 'Lim', 'surname': 'Bok', 'age': 12}
+    print((my_dictionary['surname']) + ' ' +(my_dictionary['name']))
+BokLi()
+
+
+
+
+
+
+#функция createObjFromStr
+def createObjFromStr(string):
+    try:
+        string = string[1:-1]
+        string = string.replace(' ', ' ')
+        string = string.replace(" ", " ")
+        key_value = string.split(',')
+        dictionary = {}
+        for i in key_value:
+            key, value = i.split(':')
+            dictionary[key] = value
+        return dictionary
+    except:
+        return 'ERROR'
+print(createObjFromStr("{name: 'Vova', age: 12, type: 'owner'}"))
