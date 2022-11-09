8e4ef94378e58119f5edaefdc2bdfb7701eaf733
vladislove <115663088+piligrimfox@users.noreply.github.com>
2022-11-10 00:32:51 +0300

    Update main.py

--- release-2/main.py
+++ release-2/main.py
@@ -1,2 +1,36 @@
 def sorting(arr, direction):
     return 
+
+
+
+
+
+
+
+
+
+
+
+
+
+#функция min
+def Min():
+    arr=[1, 5, 34, -100]
+    smallest = min(arr)
+    print(smallest)
+Min()
+
+
+
+
+#функция getSet
+numbers = [1, 1, 1, 3, 4, 2, 2]
+def getSet(numbers):
+    list_of_unique_numbers = []
+    unique_numbers = set(numbers)
+    for number in unique_numbers:
+        list_of_unique_numbers.append(number)
+
+    return list_of_unique_numbers
+
+print(getSet(numbers))
